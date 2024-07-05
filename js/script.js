5e47a75dd355aaa761c0b439bc77bd9f7d5e7065
victorygrey <muhfaizalhamzah@gmail.com>
2024-07-05 16:55:17 +0700

    beres bagian Fitur Search UI

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-// Toggle class active
+// Toggle class active Hamburger
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger-menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// klik di luar sidebar buat ngilangin navbar nya
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle class active Search Form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+// ketika search-icon di klik
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+// klik di luar element
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function(e) {
-    if (!hamburger.contains(e.target) &&!navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) &&!navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+    if (!sb.contains(e.target) &&!searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 })
