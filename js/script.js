d04d9c28db94c1b7d44f615c2fbd1812902c60ad
victorygrey <muhfaizalhamzah@gmail.com>
2024-07-09 21:27:50 +0700

    beres tambah fitur produk, detail produk, keranjang, (UI)

--- js/script.js
+++ js/script.js
@@ -1,50 +1,66 @@
 // Toggle class active Hamburger
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger-menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
 // Toggle class active Search Form
-const searchForm = document.querySelector('.search-form');
-const searchBox = document.querySelector('#search-box');
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 // ketika search-icon di klik
-document.querySelector('#search-button').onclick = (e) => {
-    searchForm.classList.toggle('active');
-    searchBox.focus();
-    e.preventDefault();
-}
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 
 // Toggle class active Shopping Cart
-const shoppingCart = document.querySelector('.shopping-cart');
-document.querySelector('#shopping-cart').onclick = (e) => {
-    shoppingCart.classList.toggle('active');
-    e.preventDefault();
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
 };
 
 // klik di luar element
-const hm = document.querySelector('#hamburger-menu');
-const sb = document.querySelector('#search-button');
-const sc = document.querySelector('#shopping-cart');
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart");
 
-document.addEventListener('click', function(e) {
-    if (!hm.contains(e.target) &&!navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
-    if (!sb.contains(e.target) &&!searchForm.contains(e.target)) {
-        searchForm.classList.remove('active');
-    }
-    if (!sc.contains(e.target) &&!shoppingCart.contains(e.target)) {
-        shoppingCart.classList.remove('active');
-    }
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
 
 // Modal Box
 
-const itemDetailModal = document.querySelector('#item-detail-modal');
-const itemDetailButton = document.querySelector('.item-detail-button');
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
 
-itemDetailButton.onclick = (e) => {
-    itemDetailModal.style.display = 'flex';
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
     e.preventDefault();
+  }
+});
+
+// klik tombol close
+
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
 }
